882e81e92e23645c38d175aea95bc9b5dd3c6e30
Shme1ev <50120018+Shme1ev@users.noreply.github.com>
2019-04-30 09:53:08 +0300

    Add files via upload

---
+++ pr1_v1.cpp
@@ -0,0 +1,51 @@
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#include <conio.h>
+
+using namespace std;
+
+bool chk(string str)
+{
+	int count = 0;
+	for (int i = 0; i < str.length(); i++)
+	{
+		if ((int(str[i]) > 64) && (int(str[i]) < 123))
+		{
+			count++;
+		}
+	}
+	if (count == str.length())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+int main()
+{
+	string str;
+	int kkk = 0;
+	setlocale(LC_ALL, "Rus");
+	cout << "Ведите Ваше имя: ";
+	cin >> str;
+
+	if (chk(str) == false)
+	{
+		throw 0;
+	}
+
+	else
+	{
+		cout << "Привет, " << str << endl;
+	}
+
+	_getch();
+	cin.get();
+	return 0;
+}
